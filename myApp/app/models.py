5ed04dfe85b837592216ffec91aa87e0286dd64d
valmir <valmirchagas88@gmail.com>
2023-12-28 16:32:46 -0300

    feat: add image-name and image_url field models

--- myApp/app/models.py
+++ myApp/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class MyFile(models.Model):
+    image_name = models.CharField(max_length=100, default="image")
+    image_url = models.ImageField(blank=True, null=True, upload_to="img/")
+
+    def __str__(self):
+        return self.image_name
