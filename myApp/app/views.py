674d5d10b5c58597a1e2a7e61f4d55cd312311ce
valmir <valmirchagas88@gmail.com>
2023-12-28 17:05:05 -0300

    feat: add feature to save image at img folder and url at model

--- myApp/app/views.py
+++ myApp/app/views.py
@@ -4,19 +4,36 @@ import os
 from django.conf import settings
 import datetime
 from django.http import HttpResponse
+from .models import MyFile
+
+
+# !!!!!!!  Method to save image at a remote folder!   !!!!!
+
+
+# def app(request):
+#     if request.method == "GET":
+#         return render(request, "index.html")
+#     elif request.methot == "POST":
+#         file = request.FILES.get("my_file")
+#         img = Image.open(file)
+#         img_resized = img.resize((200, 200))
+#         path = os.path.join(
+#             settings.BASE_DIR,
+#             f"media/{datetime.datetime.now()}-{file.name}"
+#         )
+#         img = img_resized.save(path)
+
+#         return HttpResponse("Imagem salva com Sucesso!")
 
 
 def app(request):
     if request.method == "GET":
-        return render(request, index.html)
-    elif request.methot == "POST":
+        return render(request, "index.html")
+    elif request.method == "POST":
         file = request.FILES.get("my_file")
-        img = Image.open(file)
-        img_resized = img.resize((200, 200))
-        path = os.path.join(
-            settings.BASE_DIR,
-            f"media/{datetime.datetime.now()}-{file.name}"
-        )
-        img = img_resized.save(path)
+        image_name = "{}-{}".format(datetime.datetime.now(), file.name)
+
+        img = MyFile(image_name=image_name, image_url=file)
+        img.save()
 
         return HttpResponse("Imagem salva com Sucesso!")
