9c0ea3b63f3b65387f0477b56267b7c4b10ece8b
valmir <valmirchagas88@gmail.com>
2023-12-28 19:25:44 -0300

    chore: add local host to access api

--- myApp/myApp/settings.py
+++ myApp/myApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-$j35_))423pmq(bejmjg2t=qi(ber#z907j7j71mlepdaw)nrv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
