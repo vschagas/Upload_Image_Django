5af3505324a2cdfe5b91066cfb999b8fb4044a7d
valmir <valmirchagas88@gmail.com>
2023-12-28 16:51:00 -0300

    feat: add app feature to save image at remote folder

--- myApp/app/views.py
+++ myApp/app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from PIL import Image
+import os
+from django.conf import settings
+import datetime
+from django.http import HttpResponse
 
-# Create your views here.
+
+def app(request):
+    if request.method == "GET":
+        return render(request, index.html)
+    elif request.methot == "POST":
+        file = request.FILES.get("my_file")
+        img = Image.open(file)
+        img_resized = img.resize((200, 200))
+        path = os.path.join(
+            settings.BASE_DIR,
+            f"media/{datetime.datetime.now()}-{file.name}"
+        )
+        img = img_resized.save(path)
+
+        return HttpResponse("Imagem salva com Sucesso!")
